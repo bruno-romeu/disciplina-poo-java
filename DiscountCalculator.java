6b91e7f8768ac030ce78dcbe5ddaa31cf20d6a9e
bruno <bruno.rsilva2004@gmail.com>
2025-09-04 16:00:12 -0300

    feat: Implementa a Calculadora de Desconto
    Cria a Classe "DiscountCalculator.java" que utiliza a classe Scanner
    para receber o valor de algo e a sua porcentagem de desconto.
    
    O programa calcula e exibe no terminal o valor total do item, a
    porcentagem de desconto, o valor do desconto aplicado e o valor final
    com o desconto, concluindo o requisito da primeira atividade da
    disciplina.

--- DiscountCalculator.java
+++ DiscountCalculator.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
-
-public class CalcDesc {
+public class DiscountCalculator {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
 
         double value, discount;
-
+        
         System.out.println("Digite um valor: ");
         value = scanner.nextDouble();
 
@@ -14,14 +13,15 @@ public class CalcDesc {
         discount = scanner.nextDouble();
 
         double calc = value * (discount / 100);
-
         double finalValue = value - calc;
 
         System.out.println("Valor total: R$" + value);
-        System.out.println("Desconto aplicado: R$" + discount + "%");
-        System.out.println("Valor do desconto: R$" + calc); 
+        System.out.println("Desconto aplicado: " + discount + "%");
+        System.out.println("Valor do desconto: R$" + calc);
         System.out.println("Valor Final com Desconto Aplicado: R$" + finalValue);
 
+        scanner.close();
 
     }
+
 }
